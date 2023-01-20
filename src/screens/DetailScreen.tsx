7a62df371ac40fe9895efd5a041babcf39e8dda5
JohnyVB <johnyvgoog@gmail.com>
2023-01-19 22:46:54 -0500

    add back button

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { ActivityIndicator, Dimensions, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Dimensions, Image, ScrollView, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/Navigation';
 import { useMovieDetails } from '../hooks/useMovieDetails';
 import { MovieDetails } from '../components/MovieDetails';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'>{}
 
 const {height: screenHeight} = Dimensions.get('screen');
 
-export const DetailScreen = ({route}: Props) => {
+export const DetailScreen = ({route, navigation}: Props) => {
 
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
@@ -37,6 +38,19 @@ export const DetailScreen = ({route}: Props) => {
           ? <ActivityIndicator size={35} color="grey" style={{marginTop: 20}} />
           : <MovieDetails details={details!} cast={cast} />
       }
+
+      {/* Boton cerrar */}
+      <TouchableOpacity
+        style={styles.backButton}
+        activeOpacity={0.8}
+        onPress={() => navigation.pop()}
+      >
+        <Icon 
+          color="grey"
+          name="chevron-back-circle"
+          size={50}
+        />
+      </TouchableOpacity>
      
     </ScrollView>
   )
@@ -79,5 +93,10 @@ const styles = StyleSheet.create({
       fontSize: 20,
       fontWeight: 'bold',
       color: 'black'
+    },
+    backButton: {
+      position: 'absolute',
+      top: 40,
+      left: 5
     }
 });
